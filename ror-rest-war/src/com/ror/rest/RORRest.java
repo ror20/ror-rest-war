81539ee5ed90547d99e9b993579329ffdd6e8bfe
ror20 <38526358+ror20@users.noreply.github.com>
2018-04-21 17:26:08 +0530

    rest sample check

--- ror-rest-war/src/com/ror/rest/RORRest.java
+++ ror-rest-war/src/com/ror/rest/RORRest.java
@@ -1,5 +1,15 @@
 package com.ror.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+@Path("rest")
 public class RORRest {
+	@GET
+	@Path("/sample")
+	public String sampleRest() {
+		return "Rest Working Fine!!";
+	}
 
 }
